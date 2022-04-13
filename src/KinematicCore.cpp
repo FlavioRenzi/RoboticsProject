b18198e3e4fa6c1dcec8fcbd8a51cbd718b0ae3b
Eliaxie <elia.maggioni@gmail.com>
2022-04-13 16:15:53 +0200

    matrix

--- src/KinematicCore.cpp
+++ src/KinematicCore.cpp
@@ -4,7 +4,10 @@
 #include <sstream>
 
 void RigidBodyCalc(const RoboticsProject::WheelSpeed &msg){
-    
+    double l1 = 10;
+    double l2 = 20;
+    double K[3][4] = {{1,1,1,1}, {1,-1,-1,1},{-1/(l1+l2), 1/(l1+l2), -1/(l1+l2), 1/(l1+l2)}};
+    vx = 
     //publish
     pub.publish(wheelSpeedMsg);
 }
